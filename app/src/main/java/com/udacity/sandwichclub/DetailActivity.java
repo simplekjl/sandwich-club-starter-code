d36ffd1b334630c8287383ee0e4045be63afbc1b
Jose L Crisostomo <jl.cs606@gmail.com>
2019-01-23 09:54:33 -0800

    *Json Util working *view working

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
@@ -14,41 +15,27 @@ public class DetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
+    private int position = -1;
+    private Sandwich sandwich = null;
+    private TextView descriptionTv;
+    private ImageView ingredientsIv;
+    private TextView originTv;
+    private TextView alsoKnownTv;
+    private TextView ingredientsTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        validateExtras();
+        ingredientsIv = findViewById(R.id.image_iv);
+        originTv = findViewById(R.id.origin_label);
+        alsoKnownTv = findViewById(R.id.also_known_tv);
+        ingredientsTv = findViewById(R.id.ingredients_label);
+        descriptionTv = findViewById(R.id.description_label);
+        getSandwichDetails();
+        populateUI(sandwich);
 
-        ImageView ingredientsIv = findViewById(R.id.image_iv);
-
-        Intent intent = getIntent();
-        if (intent == null) {
-            closeOnError();
-        }
-
-        int position = intent.getIntExtra(EXTRA_POSITION, DEFAULT_POSITION);
-        if (position == DEFAULT_POSITION) {
-            // EXTRA_POSITION not found in intent
-            closeOnError();
-            return;
-        }
-
-        String[] sandwiches = getResources().getStringArray(R.array.sandwich_details);
-        String json = sandwiches[position];
-        Sandwich sandwich = JsonUtils.parseSandwichJson(json);
-        if (sandwich == null) {
-            // Sandwich data unavailable
-            closeOnError();
-            return;
-        }
-
-        populateUI();
-        Picasso.with(this)
-                .load(sandwich.getImage())
-                .into(ingredientsIv);
-
-        setTitle(sandwich.getMainName());
     }
 
     private void closeOnError() {
@@ -56,7 +43,44 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    private void getSandwichDetails() {
+        String[] sandwiches = getResources().getStringArray(R.array.sandwich_details);
+        String json = sandwiches[position];
+        sandwich = JsonUtils.parseSandwichJson(json);
+        if (sandwich == null) {
+            // Sandwich data unavailable
+            closeOnError();
+            return;
+        }
+    }
 
+    private void populateUI(Sandwich sandwich) {
+        Picasso.with(this)
+                .load(sandwich.getImage())
+                .into(ingredientsIv);
+        setTitle(sandwich.getMainName());
+        //sandwich details
+        originTv.setText(sandwich.getPlaceOfOrigin());
+        for(String name : sandwich.getAlsoKnownAs()){
+            alsoKnownTv.append(name + ",");
+        }
+
+        ingredientsTv.setText(sandwich.getIngredients().toString());
+        descriptionTv.setText(sandwich.getDescription());
+    }
+
+    private void validateExtras() {
+
+        Intent intent = getIntent();
+        if (intent == null) {
+            closeOnError();
+        }
+
+        position = intent.getIntExtra(EXTRA_POSITION, DEFAULT_POSITION);
+        if (position == DEFAULT_POSITION) {
+            // EXTRA_POSITION not found in intent
+            closeOnError();
+            return;
+        }
     }
 }
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,9 +2,42 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class JsonUtils {
 
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+
+        Sandwich sandwich = new Sandwich();
+        try {
+            JSONObject sandwichJson = new JSONObject(json);
+            List<String> namesList = new ArrayList<>();
+            JSONArray names = sandwichJson.getJSONObject("name").getJSONArray("alsoKnownAs");
+            for(int i=0 ; i<names.length(); i++){
+                namesList.add(names.getString(i));
+            }
+            sandwich.setMainName(sandwichJson.getJSONObject("name").getString("mainName"));
+            sandwich.setAlsoKnownAs(namesList);
+            sandwich.setImage(sandwichJson.getString("image"));
+            List<String> ingredientsList = new ArrayList<>();
+            JSONArray ingredients = sandwichJson.getJSONArray("ingredients");
+            for(int i=0 ; i<ingredients.length(); i++){
+                namesList.add(ingredients.getString(i));
+            }
+            sandwich.setIngredients(ingredientsList);
+            sandwich.setPlaceOfOrigin(sandwichJson.getString("placeOfOrigin"));
+            sandwich.setDescription(sandwichJson.getString("description"));
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return sandwich;
+
     }
 }
